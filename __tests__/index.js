54944ce204300a1c961f934e783cb7465f47d83b
twinh <twinhuang@qq.com>
2021-07-02 17:13:51 +0800

    test(m-rich-text): 增加 `RichText` 组件

---
+++ __tests__/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import RichText from '..';
+import {render} from '@testing-library/react';
+
+describe('RichText', () => {
+  test('basic', () => {
+    const result = render(<RichText>test</RichText>);
+    expect(result.container).toMatchSnapshot();
+  });
+
+  test('html', () => {
+    const content = '<p>123</p><p><img src="test"/></p>';
+    const result = render(<RichText>{content}</RichText>);
+    expect(result.container).toMatchSnapshot();
+  });
+});
